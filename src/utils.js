8b9556dff627e8fed200390d26c31cb83aab2d38
ye-yo <yeyo0x0@gmail.com>
2022-05-13 10:55:55 +0900

    Fix: index calculation

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,15 @@
 const setInfiniteSlide = (datas, slideToAdd) => {
   const newSlides = [...datas];
+  const size = datas.length;
   for (let i = 0; i < slideToAdd; i += 1) {
-    const first = { ...datas[i], id: datas.length + i };
-    const last = { ...datas[datas.length - 1 - i], id: -(i + 1) };
+    const first = {
+      ...datas[i % size],
+      id: datas.length + i,
+    };
+    const last = {
+      ...datas[datas.length - 1 - (i % size)],
+      id: -(i + 1),
+    };
     newSlides.unshift(last);
     newSlides.push(first);
   }
